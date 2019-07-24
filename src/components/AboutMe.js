5025fb2ac528b2cc3218f1123ed9587a98484695
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-07-24 10:57:42 -0500

    more goodness

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { StyledDivWrapper } from "./Styles";
 import video from "../img-video/background4.mp4";
-import logo from "../img-video/logo5.png";
+import logo from "../img-video/logo7.png";
+import java from "../img-video/social-icons/java.png";
+import react from "../img-video/social-icons/react.png";
+import git from "../img-video/social-icons/git.png";
+import node from "../img-video/social-icons/node.png";
+import express from "../img-video/social-icons/express.png";
 import "./CSS.css";
 
 export class AboutMe extends React.Component {
@@ -27,28 +32,43 @@ export class AboutMe extends React.Component {
         <div className="lower-section" />
         <nav>
           <ul>
+            <li className="twitter">
+              <a href="http://twitter.com/chasegarsee" target="_blank">
+                <i class="fab fa-twitter fa-2x" />
+              </a>
+            </li>
             <li className="github">
               <a href="http://github.com/chasegarsee" target="_blank">
                 <i class="fab fa-github fa-2x" />
               </a>
             </li>
-            <li className="twitter">
-              <a href="http://twitter.com/chasegarsee" target="_blank">
-                <i class="fab fa-twitter fa-3x" />
-              </a>
-            </li>
-            <li className="mail">
-              <a href="mailto:chasegarsee@gmail.com.com" target="_blank">
-                <i class="far fa-envelope fa-3x" />
-              </a>
-            </li>
-            <li className="mail">
-              <a href="mailto:chasegarsee@gmail.com.com" target="_blank">
+            <li className="linkedin">
+              <a
+                href="https://www.linkedin.com/in/chase-garsee-7b11a4105/"
+                target="_blank"
+              >
                 <i class="fab fa-linkedin fa-2x" />
+              </a>
+            </li>
+            <li className="mail">
+              <a href="mailto:chasegarsee@gmail.com.com" target="_blank">
+                <i class="far fa-envelope fa-2x" />
               </a>
             </li>
           </ul>
         </nav>
+        <hr />
+        <p>Full Stack Developer with a passion for proper design. </p>
+        <p>
+          Also, I <img className="git" src={git} /> gitty and over{" "}
+          <img className="react" src={react} /> when folks talk about
+          <img className="java-cup" src={java} />
+        </p>
+        <p>
+          But <img className="node" src={node} /> need to worry, that's how I
+          <img className="express" src={express} />
+          myself
+        </p>
       </div>
     );
   }

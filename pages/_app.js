6089957b37215f4f25e2d30f11a96150ebbdedce
Chase Garsee <chasegarsee@gmail.com>
2022-04-21 15:03:39 -0500

    add google optimize

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,10 @@ function MyApp({ Component, pageProps }) {
     <div className="app-container bg-white text-gray-700 dark:bg-gray-900 dark:text-white">
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
+        strategy="afterInteractive" 
+        src="https://www.googleoptimize.com/optimize.js?id=OPT-PTXJ8BQ"
+        />
+      <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />

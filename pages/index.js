e94a4b87e036adf1bf96b25cd72af2293463550d
Chase Garsee <chasegarsee@gmail.com>
2022-04-21 15:10:24 -0500

    adds in script tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Script from 'next/script'
 
 import styles from '../styles/Home.module.scss'
 import { PLCLogoLight, PLCLogoDark } from '../components/icons'

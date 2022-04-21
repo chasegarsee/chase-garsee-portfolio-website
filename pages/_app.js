2756ac16c6bb79217188b0363c7896d8377d68e5
Chase Garsee <chasegarsee@gmail.com>
2022-04-21 15:08:38 -0500

    moves to correct location

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.scss'
 
 import { useEffect } from 'react'
-import Script from 'next/script'
 import { useRouter } from 'next/router'
 import * as gtag from '../library/gtag'
 
@@ -29,28 +28,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="app-container bg-white text-gray-700 dark:bg-gray-900 dark:text-white">
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive" 
-        src="https://www.googleoptimize.com/optimize.js?id=OPT-PTXJ8BQ"
-        />
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Navigation colorTheme={colorTheme} setTheme={setTheme} />
       <Component colorTheme={colorTheme} {...pageProps} />
       <Footer />

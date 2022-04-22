680ff47e674ed02a6cf9b4b94625e433a30213d2
Chase Garsee <chasegarsee@gmail.com>
2022-04-22 08:25:20 -0500

    Put it back...

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,24 @@ function MyApp({ Component, pageProps }) {
         <Script
           strategy="afterInteractive" 
           src="https://www.googleoptimize.com/optimize.js?id=OPT-PTXJ8BQ"
+          />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
         />
       </Head>
       <Navigation colorTheme={colorTheme} setTheme={setTheme} />
